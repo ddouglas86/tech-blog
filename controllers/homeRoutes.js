a7b84692f9bfbdead136b858ba7b3e775814f185
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-11-20 01:06:16 -0600

    added loggedin route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -93,3 +93,13 @@ router.get('/post/:id', (req, res) => {
     console.log(err);
     res.status(500).json(err);
 });
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+module.exports = router;
