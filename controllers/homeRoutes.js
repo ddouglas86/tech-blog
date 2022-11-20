ad174196677bb0262a030df6129c8541cdcd2434
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-11-20 02:23:56 -0600

    removed unused variable

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const { User, Post, Comment } = require("../models");
-const sequelize = require("../config/connection");
 
 router.get('/', (req, res) => {
     Post.findAll({
