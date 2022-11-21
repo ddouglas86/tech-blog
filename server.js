ee0c52829604a830fcc303060f0ec2c0feea0695
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-11-20 19:11:47 -0600

    fixed misspelling in 'false' that was throwing error

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const exphbs = require("express-handlebars");
 const sequelize = require("./config/connection");
 const session = require("express-session");
 const SequlizeStore = require("connect-session-sequelize")(session.Store);
+const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -20,7 +21,7 @@ const sess = {
 
 app.use(session(sess));
 app.use(express.json());
-app.use(express.urlencoded({ extended: flase }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers'));
 
