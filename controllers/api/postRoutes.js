3f2204bf9500ef415f1613d3d5340ed98365d653
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-11-20 18:40:51 -0600

    added update and delete routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -17,3 +17,52 @@ router.post('/', withAuth, (req, res) => {
     });
 });
 
+router.put('/:id', withAuth, (req, res) => {
+    Post.update(
+        {
+            title: req.body.title,
+            content: req.body.content
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(updatePostData => {
+        if (!updatePostData) {
+            res.status(404).json({ message: 'Invalid ID' });
+            return;
+        } else {
+            res.json(updatePostData)
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+        res.json(err);
+    })
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+    Post.destroy(
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(deletePostData => {
+        if (!deletePostData) {
+            res.status(404).json({ message: 'Invalid ID' });
+            return;
+        } else {
+            res.json(deletePostData)
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+        res.json(err);
+    })
+});
+
+module.exports = router;
