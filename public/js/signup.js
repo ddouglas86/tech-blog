ccc7231de192422b22f189e6f9075b2cfe104f66
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-11-21 17:44:40 -0600

    added signup js

---
+++ public/js/signup.js
@@ -0,0 +1,28 @@
+const signupFormHandler = async function(event) {
+    event.preventDefault();
+  
+    const usernameEl = document.querySelector('#inputUsername');
+    const emailEl = document.querySelector('#inputEmail')
+    const passwordEl = document.querySelector('#inputPassword');
+  
+    const response = await fetch('/api/user', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: usernameEl.value,
+        email: emailEl.value,
+        password: passwordEl.value,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Error signing up');
+    }
+  };
+  
+  document
+    .querySelector('#signupForm')
+    .addEventListener('submit', signupFormHandler);
+  
