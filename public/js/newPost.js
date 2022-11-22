44426cbdebb65ec27db9c937e4c61bd6fa9e2326
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-11-21 19:00:01 -0600

    added new post js

---
+++ public/js/newPost.js
@@ -0,0 +1,22 @@
+const newFormHandler = async function(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="postTitle"]').value;
+    const body = document.querySelector('textarea[name="postContent"]').value;
+  
+    await fetch(`/api/post`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        body,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    document.location.replace('/dashboard');
+  };
+  
+  document
+    .querySelector('#newPost')
+    .addEventListener('submit', newFormHandler);
+  
