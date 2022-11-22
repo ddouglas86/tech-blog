8967fc8bc591a90b4a62ffc27fab4496f593fb61
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-11-22 02:46:25 -0600

    fixed variables

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -2,7 +2,7 @@ const newFormHandler = async function(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="postTitle"]').value;
-    const body = document.querySelector('textarea[name="postContent"]').value;
+    const content = document.querySelector('textarea[name="postContent"]').value;
   
     await fetch(`/api/post`, {
       method: 'POST',
