a475f3ce248960745d2408a7b8b567bdc4e92852
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-12-04 15:32:40 -0600

    fixed Post require path

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Comment } = require('../../config/connection');
+const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, (req, res) => {
