1809ddbf5f15d262847bbd50351885d16723a2d2
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-12-04 16:37:26 -0600

    added redirect for signup

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -101,4 +101,12 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/dashboard');
+        return;
+    }
+    res.render('signup');
+});
+
 module.exports = router;
