d630a5abc409755886337b97ba162808e1fd4e37
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-12-04 18:44:18 -0600

    updated routes to include async/await

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,96 +1,93 @@
 const router = require("express").Router();
 const { User, Post, Comment } = require("../models");
 
-router.get('/', (req, res) => {
-    Post.findAll({
-        attributes: [
-            'id',
-            'title',
-            'content',
-            'date_created',
-        ],
-        include: [
-            {
-                model: User,
-                attributes: ['username']
-            },
-            {
-                model: Comment,
-                attributes: [
-                    'id',
-                    'content',
-                    'post_id',
-                    'user_id',
-                ],
-                include: {
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'date_created',
+            ],
+            include: [
+                {
                     model: User,
                     attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: [
+                        'id',
+                        'content',
+                        'post_id',
+                        'user_id',
+                    ],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
                 }
-            }
-        ]
-    })
-        .then((postData) => {
-            const allPosts = postData.map(post => post.get({ plain: true }));
-            res.render('homepage', {
-                allPosts
-            });
+            ]
         })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        })
+
+        const allPosts = postData.map(post => post.get({ plain: true }));
+        res.render('homepage', {
+            allPosts
+        });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+
 });
 
-router.get('/post/:id', (req, res) => {
-    Post.findOne({
-        where: {
-            id: req.params.id
-        },
-        attributes: [
-            'id',
-            'title',
-            'content',
-            'date_created',
-        ],
-        include: [
-            {
-                model: User,
-                attributes: [
-                    'username'
-                ]
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findOne({
+            where: {
+                id: req.params.id
             },
-            {
-                model: Comment,
-                attributes: [
-                    'id',
-                    'content',
-                    'post_id',
-                    'user_id',
-                ],
-                include: {
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'date_created',
+            ],
+            include: [
+                {
                     model: User,
                     attributes: [
                         'username'
                     ]
+                },
+                {
+                    model: Comment,
+                    attributes: [
+                        'id',
+                        'content',
+                        'post_id',
+                        'user_id',
+                    ],
+                    include: {
+                        model: User,
+                        attributes: [
+                            'username'
+                        ]
+                    }
                 }
-            }
-        ]
-    })
-        .then((postData) => {
-            if (!postData) {
-                res.status(404).json({ message: 'Invalid ID' });
-                return;
-            } else {
-                const post = postData.get({ plain: true });
-                res.render('single-post', {
-                    post
-                });
-            }
+            ]
         })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
+        const post = postData.get({ plain: true });
+        res.render('single-post', {
+            post
         });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    };
 });
 
 router.get('/login', (req, res) => {
