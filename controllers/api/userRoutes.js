ad0d6300a9d9333fa2a7a545825ef38bfccaa18b
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-12-04 18:55:44 -0600

    updated with asyc/await

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,38 +1,36 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-router.post('/', (req, res) => {
-    User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    })
-        .then((newUserData) => {
+router.post('/signup', async (req, res) => {
+    try{
+        const newUserData = await User.create(req.body)
+        
             req.session.save(() => {
                 req.session.user_id = newUserData.id;
-                req.session.username = newUserData.username;
                 req.session.loggedIn = true;
                 res.json(newUserData)
             })
-        })
-        .catch((err) => {
+       
+        }
+        catch(err) {
             res.status(500).json(err);
-        })
+        }
 });
 
-router.post('/login', (req, res) => {
-    User.findOne({
+router.post('/login', async (req, res) => {
+    try { 
+        const userData = await User.findOne({
         where: {
             email: req.body.email
         }
-    })
-        .then((userData) => {
+    });
+        
             if (!userData) {
                 res.status(400).json({ message: 'Invalid email address' });
                 return;
             }
 
-            const validPassword = userData.checkPassword(req.body.password);
+            const validPassword = await userData.checkPassword(req.body.password);
 
             if (!validPassword) {
                 res.status(400).json({ message: 'Invalid password' });
@@ -41,12 +39,13 @@ router.post('/login', (req, res) => {
 
             req.session.save(() => {
                 req.session.user_id = userData.id;
-                req.session.username = userData.username;
                 req.session.loggedIn = true;
-
                 res.json({ user: userData, message: 'You are logged in!' });
             })
-        })
+    } 
+    catch (err) {
+        res.status(400).json(err);
+    }
 });
 
 router.post('/logout', (req, res) => {
